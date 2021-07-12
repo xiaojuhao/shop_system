19a2a24f7094aec51fbc30d52d4bb3c83f34b0f5
ygl <yglah126@126.com>
2021-07-12 13:53:33 +0800

    fff

--- shop-startup/src/main/java/com/xjh/startup/LoginApp.java
+++ shop-startup/src/main/java/com/xjh/startup/LoginApp.java
@@ -18,17 +18,18 @@ public class LoginApp extends Application {
     public static Holder<XjhWebSocketServer> server = new Holder<>();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         TimeRecord timeRecord = TimeRecord.start();
-        primaryStage.setTitle("登录系统");
+
         VBox login = FxmlView.load("login");
         assert login != null;
         login.setAlignment(Pos.CENTER);
         login.setPadding(new Insets(0, 400, 0, 400));
-        LogUtils.info("加载登录界面, cost " + timeRecord.getCostAndReset());
-
         Scene loginScene = new Scene(login);
         loginScene.getStylesheets().add("/css/style.css");
+        LogUtils.info("加载登录界面, cost " + timeRecord.getCostAndReset());
+
+        primaryStage.setTitle("登录系统");
         primaryStage.setMaximized(true);
         primaryStage.setScene(loginScene);
         primaryStage.setOnHidden(evt -> server.get().stopQuietly());
