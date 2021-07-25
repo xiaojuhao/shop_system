5b42be88831033a776f530d21d1e0d0a22ecf495
yuan6331 <271345425@qq.com>
2021-07-25 23:00:54 +0800

    add

--- shop-startup/src/main/java/com/xjh/startup/view/DeskView.java
+++ shop-startup/src/main/java/com/xjh/startup/view/DeskView.java
@@ -35,14 +35,22 @@ public class DeskView extends VBox {
         this.setSpacing(10);
         this.setAlignment(Pos.CENTER);
 
+
         EnumDesKStatus status = EnumDesKStatus.of(desk.get().getStatus());
         setBackground(this, status);
         Label statusLabel = new Label(status.remark());
-        Label timeLabel = new Label();
-        timeLabel.setText(DateBuilder.base(desk.get().getOrderCreateTime()).format(TIME_FORMAT));
-        Label tableName = new Label(desk.get().getDeskName());
-        tableName.setFont(new javafx.scene.text.Font("微软雅黑", 15));
+        Label timeLabel = buildTimeLabel(desk.get());
+        Label tableName = buildTableName(desk.get());
+
         this.getChildren().addAll(tableName, statusLabel, timeLabel);
+
+        //        GridPane grid = new GridPane();
+        //        grid.setAlignment(Pos.CENTER);
+        //        grid.setHgap(20);
+        //        grid.add(statusLabel, 1, 0);
+        //        grid.add(tableName, 0, 1, 2, 1);
+        //        grid.add(timeLabel, 0, 2);
+        //        this.getChildren().add(grid);
 
         desk.addListener((cc, _old, _new) -> {
             EnumDesKStatus desKStatus = EnumDesKStatus.of(_new.getStatus());
@@ -63,40 +71,19 @@ public class DeskView extends VBox {
             }
         });
 
-        this.setOnMouseClicked(evt -> {
-            DeskService deskService = GuiceContainer.getInstance(DeskService.class);
-            EnumDesKStatus runStatus = EnumDesKStatus.of(desk.get().getStatus());
-            if (runStatus == EnumDesKStatus.USED || runStatus == EnumDesKStatus.PAID) {
-                Window sceneWindow = this.getScene().getWindow();
-                Stage orderInfo = new Stage();
-                orderInfo.initOwner(sceneWindow);
-                orderInfo.initModality(Modality.WINDOW_MODAL);
-                orderInfo.initStyle(StageStyle.DECORATED);
-                orderInfo.centerOnScreen();
-                orderInfo.setWidth(sceneWindow.getWidth() / 10 * 9);
-                orderInfo.setHeight(sceneWindow.getHeight() / 10 * 9);
-                orderInfo.setTitle("订单详情");
-                orderInfo.setScene(new Scene(new OrderDetail(desk.get())));
-                orderInfo.show();
-                System.gc();
-            } else {
-                OpenDeskDialog dialog = new OpenDeskDialog(desk.get());
-                Optional<OpenDeskInputParam> result = dialog.showAndWait();
-                if (result.isPresent() && result.get().getResult() == 1) {
-                    if (result.get().getCustomerNum() <= 0) {
-                        AlertBuilder.ERROR("请输入就餐人数");
-                        return;
-                    }
-                    OpenDeskParam openDeskParam = new OpenDeskParam();
-                    openDeskParam.setDeskId(desk.get().getDeskId());
-                    openDeskParam.setCustomerNum(result.get().getCustomerNum());
-                    Result<String> openDeskRs = deskService.openDesk(openDeskParam);
-                    if (!openDeskRs.isSuccess()) {
-                        AlertBuilder.ERROR("开桌失败", openDeskRs.getMsg());
-                    }
-                }
-            }
-        });
+        this.setOnMouseClicked(evt -> this.onClickTable(desk));
+    }
+
+    private Label buildTableName(Desk desk) {
+        Label label = new Label(desk.getDeskName());
+        label.setFont(new javafx.scene.text.Font("微软雅黑", 15));
+        return label;
+    }
+
+    private Label buildTimeLabel(Desk desk) {
+        Label timeLabel = new Label();
+        timeLabel.setText(DateBuilder.base(desk.getOrderCreateTime()).format(TIME_FORMAT));
+        return timeLabel;
     }
 
     private void setBackground(VBox vbox, EnumDesKStatus status) {
@@ -104,6 +91,41 @@ public class DeskView extends VBox {
             vbox.setStyle("-fx-background-color: #ed6871;");
         } else {
             vbox.setStyle("-fx-background-color: #228B22;");
+        }
+    }
+
+    private void onClickTable(SimpleObjectProperty<Desk> desk) {
+        DeskService deskService = GuiceContainer.getInstance(DeskService.class);
+        EnumDesKStatus runStatus = EnumDesKStatus.of(desk.get().getStatus());
+        if (runStatus == EnumDesKStatus.USED || runStatus == EnumDesKStatus.PAID) {
+            Window sceneWindow = this.getScene().getWindow();
+            Stage orderInfo = new Stage();
+            orderInfo.initOwner(sceneWindow);
+            orderInfo.initModality(Modality.WINDOW_MODAL);
+            orderInfo.initStyle(StageStyle.DECORATED);
+            orderInfo.centerOnScreen();
+            orderInfo.setWidth(sceneWindow.getWidth() / 10 * 9);
+            orderInfo.setHeight(sceneWindow.getHeight() / 10 * 9);
+            orderInfo.setTitle("订单详情");
+            orderInfo.setScene(new Scene(new OrderDetail(desk.get())));
+            orderInfo.show();
+            System.gc();
+        } else {
+            OpenDeskDialog dialog = new OpenDeskDialog(desk.get());
+            Optional<OpenDeskInputParam> result = dialog.showAndWait();
+            if (result.isPresent() && result.get().getResult() == 1) {
+                if (result.get().getCustomerNum() <= 0) {
+                    AlertBuilder.ERROR("请输入就餐人数");
+                    return;
+                }
+                OpenDeskParam openDeskParam = new OpenDeskParam();
+                openDeskParam.setDeskId(desk.get().getDeskId());
+                openDeskParam.setCustomerNum(result.get().getCustomerNum());
+                Result<String> openDeskRs = deskService.openDesk(openDeskParam);
+                if (!openDeskRs.isSuccess()) {
+                    AlertBuilder.ERROR("开桌失败", openDeskRs.getMsg());
+                }
+            }
         }
     }
 
