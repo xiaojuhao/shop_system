d97de823cc81144ce93488466cb3b2e7afc90628
yuan6331 <271345425@qq.com>
2021-07-31 20:11:38 +0800

    add

--- shop-startup/src/main/java/com/xjh/startup/view/DeskView.java
+++ shop-startup/src/main/java/com/xjh/startup/view/DeskView.java
@@ -9,6 +9,7 @@ import com.xjh.common.utils.CommonUtils;
 import com.xjh.common.utils.DateBuilder;
 import com.xjh.common.utils.Result;
 import com.xjh.dao.dataobject.Desk;
+import com.xjh.dao.dataobject.Order;
 import com.xjh.service.domain.DeskService;
 import com.xjh.service.domain.OrderService;
 import com.xjh.service.domain.model.OpenDeskParam;
@@ -16,10 +17,14 @@ import com.xjh.startup.foundation.guice.GuiceContainer;
 import com.xjh.startup.view.model.OpenDeskInputParam;
 
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -29,49 +34,87 @@ public class DeskView extends VBox {
     OrderService orderService = GuiceContainer.getInstance(OrderService.class);
     static final String TIME_FORMAT = "HH:mm:ss";
 
-    public DeskView(SimpleObjectProperty<Desk> desk, double prefWidth) {
-        this.setPrefSize(prefWidth, prefWidth / 2);
+    public DeskView(SimpleObjectProperty<Desk> desk, double width) {
+        double height = width / 2;
+        this.setPrefSize(width, height);
         this.getStyleClass().add("desk");
-        this.setSpacing(10);
-        this.setAlignment(Pos.CENTER);
-
-
+        // this.setSpacing(10);
+        // this.setAlignment(Pos.CENTER);
         EnumDesKStatus status = EnumDesKStatus.of(desk.get().getStatus());
-        setBackground(this, status);
-        Label statusLabel = new Label(status.remark());
-        Label timeLabel = buildTimeLabel(desk.get());
-        Label tableName = buildTableName(desk.get());
 
-        this.getChildren().addAll(tableName, statusLabel, timeLabel);
+        Canvas canvas = new Canvas();
+        canvas.setWidth(width);
+        canvas.setHeight(height);
+        setBackground(canvas, status);
+        canvas.setOnMouseClicked(evt -> this.onClickTable(desk));
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.save();
+        // 状态
+        gc.setFill(Color.PURPLE);
+        gc.fillRect(width / 2, 0, width / 2, 25);
+        gc.setFill(Color.WHITE);
+        double statusOffset = width - CommonUtils.length(status.remark()) * 10 - 30;
+        gc.fillText(status.remark(), statusOffset, 16);
+        gc.restore();
+        // 桌号
+        gc.setFill(Color.WHITE);
+        gc.setFont(Font.font(16));
+        gc.fillText(desk.get().getDeskName(),
+                width / 2 - CommonUtils.length(desk.get().getDeskName()) * 5,
+                height / 2);
+        gc.restore();
 
-        //        GridPane grid = new GridPane();
-        //        grid.setAlignment(Pos.CENTER);
-        //        grid.setHgap(20);
-        //        grid.add(statusLabel, 1, 0);
-        //        grid.add(tableName, 0, 1, 2, 1);
-        //        grid.add(timeLabel, 0, 2);
-        //        this.getChildren().add(grid);
+        this.getChildren().addAll(canvas);
+
+        //        Label tableName = buildTableName(desk.get());
+        //        Label statusLabel = new Label(status.remark());
+        //        Label timeLabel = buildTimeLabel(desk.get());
+        //        setBackground(this, status);
+
+        // this.getChildren().addAll(tableName, statusLabel, timeLabel);
 
         desk.addListener((cc, _old, _new) -> {
             EnumDesKStatus desKStatus = EnumDesKStatus.of(_new.getStatus());
             setBackground(this, desKStatus);
-            statusLabel.setText(desKStatus.remark());
+            // statusLabel.setText(desKStatus.remark());
+
+            Order order = orderService.getOrder(_new.getOrderId());
 
             Result<LocalDateTime> firstSubOrderTimeRs = orderService.firstSubOrderTime(_new.getOrderId());
             if (!firstSubOrderTimeRs.isSuccess()) {
                 AlertBuilder.ERROR(firstSubOrderTimeRs.getMsg());
                 return;
             }
+            String time = "";
             LocalDateTime firstSubOrderTime = firstSubOrderTimeRs.getData();
             if (firstSubOrderTime != null && desKStatus != EnumDesKStatus.FREE) {
                 long usedSeconds = DateBuilder.intervalSeconds(firstSubOrderTime, LocalDateTime.now());
-                timeLabel.setText("已用" + CommonUtils.formatSeconds(usedSeconds));
+                // timeLabel.setText("已用" + CommonUtils.formatSeconds(usedSeconds));
+                time = CommonUtils.formatSeconds(usedSeconds);
             } else if (firstSubOrderTime == null) {
-                timeLabel.setText("未点菜");
+                // timeLabel.setText("未点菜");
+                time = "未点菜";
+            }
+            // 用餐时间
+            gc.save();
+            gc.clearRect(0, height - 20, width, 20);
+            gc.setFont(Font.font(12));
+            gc.setFill(Color.WHITE);
+            gc.fillText(time, 0, height - 5);
+            gc.restore();
+            // 人数
+            if (order != null) {
+                gc.save();
+                gc.setFill(Color.GRAY);
+                gc.fillOval(width - 20, height - 20, 20, 20);
+                gc.setFill(Color.WHITE);
+                gc.setFont(Font.font(11));
+                gc.fillText(order.getOrderCustomerNums() + "", width - 14, height - 6);
+                gc.restore();
             }
         });
 
-        this.setOnMouseClicked(evt -> this.onClickTable(desk));
+        // this.setOnMouseClicked(evt -> this.onClickTable(desk));
     }
 
     private Label buildTableName(Desk desk) {
@@ -86,11 +129,13 @@ public class DeskView extends VBox {
         return timeLabel;
     }
 
-    private void setBackground(VBox vbox, EnumDesKStatus status) {
-        if (status == EnumDesKStatus.USED || status == EnumDesKStatus.PAID) {
-            vbox.setStyle("-fx-background-color: #ed6871;");
+    private void setBackground(Node node, EnumDesKStatus status) {
+        if (status == EnumDesKStatus.USED) {
+            node.setStyle("-fx-background-color: #DC143C;");
+        } else if (status == EnumDesKStatus.PAID) {
+            node.setStyle("-fx-background-color: #00bfff;");
         } else {
-            vbox.setStyle("-fx-background-color: #228B22;");
+            node.setStyle("-fx-background-color: #228B22;");
         }
     }
 
