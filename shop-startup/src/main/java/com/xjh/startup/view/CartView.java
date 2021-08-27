b61c5b0ce1355aabd4e053712ee152816df962c3
yuan6331 <271345425@qq.com>
2021-08-27 22:02:43 +0800

    add

--- shop-startup/src/main/java/com/xjh/startup/view/CartView.java
+++ shop-startup/src/main/java/com/xjh/startup/view/CartView.java
@@ -1,5 +1,12 @@
 package com.xjh.startup.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
 import com.xjh.common.utils.AlertBuilder;
 import com.xjh.common.utils.CommonUtils;
 import com.xjh.common.utils.Logger;
@@ -17,108 +24,51 @@ import com.xjh.service.domain.model.CartVO;
 import com.xjh.service.domain.model.PlaceOrderFromCartReq;
 import com.xjh.startup.view.model.CartItemBO;
 import com.xjh.startup.view.model.DeskOrderParam;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class CartView extends VBox {
     CartService cartService = GuiceContainer.getInstance(CartService.class);
     DishesTypeService dishesTypeService = GuiceContainer.getInstance(DishesTypeService.class);
     DishesDAO dishesDAO = GuiceContainer.getInstance(DishesDAO.class);
 
-    private final DeskOrderParam param;
-    private final TableView<CartItemBO> tv = new TableView<>();
-
     public CartView(DeskOrderParam param) {
-        this.param = param;
-        this.getChildren().add(tableList());
-        this.getChildren().add(separator());
-        this.getChildren().add(functions());
+        TableView<CartItemBO> tv = new TableView<>();
+        this.getChildren().add(tableList(param, tv));
+        this.getChildren().add(new Separator(Orientation.HORIZONTAL));
+        this.getChildren().add(buttons(param, tv));
     }
 
-    private HBox functions() {
-        Integer deskId = param.getDeskId();
-
+    private HBox buttons(DeskOrderParam param, TableView<CartItemBO> tv) {
         HBox box = new HBox();
         box.setAlignment(Pos.CENTER);
         box.setSpacing(20);
-        Button remove = new Button("删除");
-        Button place = new Button("确定下单");
-        box.getChildren().add(remove);
-        box.getChildren().add(place);
-        remove.setOnMouseClicked(evt -> {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("删除购物车");
-            alert.setContentText("确定要删除这行记录吗?");
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() != ButtonType.OK) {
-                return;
-            }
 
-            List<String> removeItems = new ArrayList<>();
-            ObservableList<CartItemBO> list = tv.getSelectionModel().getSelectedItems();
-            list.forEach(x -> removeItems.add(x.getDishesId() + ""));
-            if (CommonUtils.isEmpty(removeItems)) {
-                AlertBuilder.ERROR("删除购物车失败", "请选择删除记录");
-                return;
-            }
-            Result<CartVO> getCartRs = cartService.getCartOfDesk(deskId);
-            if (!getCartRs.isSuccess()) {
-                AlertBuilder.ERROR("获取购物车信息失败", getCartRs.getMsg());
-                return;
-            }
-            CartVO cartVO = getCartRs.getData();
-            if (cartVO == null || CommonUtils.isEmpty(cartVO.getContents())) {
-                return;
-            }
-            List<CartItemVO> items = cartVO.getContents();
-            items = items.stream()
-                    .filter(it -> !removeItems.contains(it.getDishesId() + ""))
-                    .collect(Collectors.toList());
-            cartVO.setContents(items);
-            Result<CartVO> updateRs = cartService.updateCart(deskId, cartVO);
-            if (updateRs.isSuccess()) {
-                reloadData();
-                // AlertBuilder.INFO("删除成功");
-            } else {
-                AlertBuilder.ERROR("删除失败," + updateRs.getMsg());
-            }
-        });
-        place.setOnMouseClicked(evt -> {
-            PlaceOrderFromCartReq req = new PlaceOrderFromCartReq();
-            req.setDeskId(deskId);
-            req.setOrderId(param.getOrderId());
-            Result<String> createOrderRs = cartService.createOrder(req);
-            if (createOrderRs.isSuccess()) {
-                AlertBuilder.INFO("下单成功");
-                this.getScene().getWindow().hide();
-            } else {
-                AlertBuilder.ERROR(createOrderRs.getMsg());
-            }
-        });
+        Button removeBtn = new Button("删除");
+        removeBtn.setOnMouseClicked(evt -> doDeleteItem(param, tv));
+
+        Button placeBtn = new Button("确定下单");
+        placeBtn.setOnMouseClicked(evt -> doPlaceOrder(param));
+
+        box.getChildren().addAll(removeBtn, placeBtn);
         return box;
     }
 
-    private Separator separator() {
-        Separator s = new Separator();
-        s.setOrientation(Orientation.HORIZONTAL);
-        return s;
-    }
-
-    private TableView<CartItemBO> tableList() {
+    private TableView<CartItemBO> tableList(DeskOrderParam param, TableView<CartItemBO> tv) {
         try {
             tv.getColumns().addAll(
                     newCol("序号", "seqNo", 100),
@@ -128,20 +78,73 @@ public class CartView extends VBox {
                     newCol("数量", "nums", 100),
                     newCol("小计", "totalPrice", 100)
             );
-            reloadData();
+            reloadData(param, tv);
         } catch (Exception ex) {
             Logger.error("查询购物车异常:" + param.getDeskName() + ", " + ex.getMessage());
         }
         return tv;
     }
 
-    private void reloadData() {
-        ObservableList<CartItemBO> observableList = FXCollections.observableList(loadCartItems());
-        tv.setItems(observableList);
+    private void reloadData(DeskOrderParam param, TableView<CartItemBO> tv) {
+        tv.setItems(FXCollections.observableList(loadCartItems(param)));
         tv.refresh();
     }
 
-    private List<CartItemBO> loadCartItems() {
+    private void doPlaceOrder(DeskOrderParam param) {
+        PlaceOrderFromCartReq req = new PlaceOrderFromCartReq();
+        req.setDeskId(param.getDeskId());
+        req.setOrderId(param.getOrderId());
+        Result<String> createOrderRs = cartService.createOrder(req);
+        if (createOrderRs.isSuccess()) {
+            AlertBuilder.INFO("下单成功");
+            this.getScene().getWindow().hide();
+        } else {
+            AlertBuilder.ERROR(createOrderRs.getMsg());
+        }
+    }
+
+    private void doDeleteItem(DeskOrderParam param, TableView<CartItemBO> tv) {
+        Integer deskId = param.getDeskId();
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("删除购物车");
+        alert.setContentText("确定要删除这行记录吗?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() != ButtonType.OK) {
+            return;
+        }
+
+        List<String> removeItems = new ArrayList<>();
+        ObservableList<CartItemBO> list = tv.getSelectionModel().getSelectedItems();
+        list.forEach(x -> removeItems.add(x.getDishesId() + ""));
+        if (CommonUtils.isEmpty(removeItems)) {
+            AlertBuilder.ERROR("删除购物车失败", "请选择删除记录");
+            return;
+        }
+        Result<CartVO> getCartRs = cartService.getCartOfDesk(deskId);
+        if (!getCartRs.isSuccess()) {
+            AlertBuilder.ERROR("获取购物车信息失败", getCartRs.getMsg());
+            return;
+        }
+        CartVO cartVO = getCartRs.getData();
+        if (cartVO == null || CommonUtils.isEmpty(cartVO.getContents())) {
+            return;
+        }
+        List<CartItemVO> items = cartVO.getContents();
+        items = items.stream()
+                .filter(it -> !removeItems.contains(it.getDishesId() + ""))
+                .collect(Collectors.toList());
+        cartVO.setContents(items);
+        Result<CartVO> updateRs = cartService.updateCart(deskId, cartVO);
+        if (updateRs.isSuccess()) {
+            reloadData(param, tv);
+            // AlertBuilder.INFO("删除成功");
+        } else {
+            AlertBuilder.ERROR("删除失败," + updateRs.getMsg());
+        }
+    }
+
+    private List<CartItemBO> loadCartItems(DeskOrderParam param) {
         try {
             Map<Integer, DishesType> typeMap = dishesTypeService.dishesTypeMap();
             List<CartItemVO> list = cartService.selectByDeskId(param.getDeskId());
