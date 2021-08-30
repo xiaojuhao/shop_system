586448380f56c9fe21d503a9f37ba60f1c99214b
yuan6331 <271345425@qq.com>
2021-08-30 21:46:59 +0800

    add

--- shop-common/src/main/java/com/xjh/common/store/SequenceDatabase.java
+++ shop-common/src/main/java/com/xjh/common/store/SequenceDatabase.java
@@ -1,35 +1,26 @@
 package com.xjh.common.store;
 
-import com.sleepycat.je.DatabaseEntry;
 import com.xjh.common.kvdb.Committable;
 import com.xjh.common.kvdb.cart.SeqKvDB;
 import com.xjh.common.utils.CommonUtils;
 import com.xjh.common.utils.Logger;
 
-import java.nio.charset.StandardCharsets;
-
 public class SequenceDatabase {
     static SeqKvDB seqKvDB = SeqKvDB.inst();
 
     public static synchronized int nextId(String group) {
-        String key = "sequence_" + group;
         Committable committable = seqKvDB.beginTransaction();
-        int newId;
         try {
-            String value = seqKvDB.get(key, String.class);
-            if (CommonUtils.isNotBlank(value)) {
-                newId = CommonUtils.parseInt(value, 1);
-            } else {
-                newId = 1;
-            }
+            String key = "sequence_" + group;
+            int newId = CommonUtils.parseInt(seqKvDB.get(key, String.class), 1);
             seqKvDB.put(key, String.valueOf(newId + 1));
+            Logger.info("创建序列号:" + group + ", 返回ID:" + newId);
+            return newId;
         } catch (Exception ex) {
             Logger.error("获取订单ID失败:" + group + "," + ex.getMessage());
             throw new RuntimeException("获取订单ID序列失败");
         } finally {
             seqKvDB.commit(committable);
         }
-        Logger.info("创建序列号:" + group + ", 返回ID:" + newId);
-        return newId;
     }
 }
