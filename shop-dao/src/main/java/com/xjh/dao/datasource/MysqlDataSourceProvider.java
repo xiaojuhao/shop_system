41f2f3f8d82d04a1e7f9cd09059d97dbf3421409
ygl <yglah126@126.com>
2021-09-01 10:47:50 +0800

    add

--- shop-dao/src/main/java/com/xjh/dao/datasource/MysqlDataSourceProvider.java
+++ shop-dao/src/main/java/com/xjh/dao/datasource/MysqlDataSourceProvider.java
@@ -1,7 +1,6 @@
 package com.xjh.dao.datasource;
 
 import com.google.inject.Provider;
-import com.xjh.common.enumeration.EnumPropName;
 import com.xjh.common.store.SysConfigUtils;
 import com.xjh.common.utils.Logger;
 import com.xjh.common.utils.TimeRecord;
@@ -9,6 +8,8 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.util.Properties;
+
+import static com.xjh.common.enumeration.EnumPropName.*;
 
 public class MysqlDataSourceProvider implements Provider<HikariDataSource> {
     private static class Inst {
@@ -22,20 +23,20 @@ public class MysqlDataSourceProvider implements Provider<HikariDataSource> {
 
     private static HikariDataSource newDS() {
         Properties properties = SysConfigUtils.getDbConfig();
-        String url = properties.getProperty(EnumPropName.DB_URL.name);
-        String driver = properties.getProperty(EnumPropName.DB_DRIVER.name);
-        String username = properties.getProperty(EnumPropName.DB_USERNAME.name);
-        String password = properties.getProperty(EnumPropName.DB_PASSWORD.name);
+        String url = properties.getProperty(DB_URL.name);
+        String driver = properties.getProperty(DB_DRIVER.name);
+        String username = properties.getProperty(DB_USERNAME.name);
+        String password = properties.getProperty(DB_PASSWORD.name);
         TimeRecord record = TimeRecord.start();
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(url);
-        hikariConfig.setDriverClassName(driver);
-        hikariConfig.setUsername(username);
-        hikariConfig.setPassword(password);
-        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
-        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        HikariDataSource ds = new HikariDataSource(hikariConfig);
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setDriverClassName(driver);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        HikariDataSource ds = new HikariDataSource(config);
         Logger.info("初始化MySql数据源: cost " + record.getCost());
         return ds;
     }
