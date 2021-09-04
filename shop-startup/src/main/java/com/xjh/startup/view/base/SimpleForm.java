b9a04b5f990a13a5544d15ad748ad156cfe9c58f
yinguoliang <yglah126@126.com>
2021-09-05 00:35:54 +0800

    add

--- shop-startup/src/main/java/com/xjh/startup/view/base/SimpleForm.java
+++ shop-startup/src/main/java/com/xjh/startup/view/base/SimpleForm.java
@@ -2,12 +2,20 @@ package com.xjh.startup.view.base;
 
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class SimpleForm extends VBox {
     public void addLine(Node node) {
         this.getChildren().add(node);
+    }
+
+    public HBox newLine(Pane title, Pane content){
+        HBox line = new HBox();
+        return line;
     }
 
     public HBox newLine(Node... nodes) {
@@ -18,5 +26,15 @@ public class SimpleForm extends VBox {
             line.getChildren().add(n);
         }
         return line;
+    }
+
+    public TextField createTextField(String promptText) {
+        TextField textField = new TextField();
+        textField.setPromptText(promptText);
+        return textField;
+    }
+
+    public Label createLabel(String text) {
+        return new Label(text);
     }
 }
--- shop-startup/src/main/java/com/xjh/startup/view/base/SmallForm.java
+++ shop-startup/src/main/java/com/xjh/startup/view/base/SmallForm.java
@@ -1,49 +1,11 @@
 package com.xjh.startup.view.base;
 
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 
 public class SmallForm extends SimpleForm {
-
     public SmallForm() {
         this.setAlignment(Pos.CENTER);
         this.setSpacing(15);
     }
 
-    public void addLine(Node node) {
-        this.getChildren().add(node);
-    }
-
-    public HBox newDefaultLine(Node... nodes) {
-        HBox line = new HBox();
-        line.setAlignment(Pos.CENTER);
-        line.setSpacing(10);
-        for (Node n : nodes) {
-            line.getChildren().add(n);
-        }
-        return line;
-    }
-
-    public HBox newLine(Node... nodes) {
-        HBox line = new HBox();
-        line.setAlignment(Pos.CENTER);
-        line.setSpacing(10);
-        for (Node n : nodes) {
-            line.getChildren().add(n);
-        }
-        return line;
-    }
-
-    public TextField createTextField(String promptText) {
-        TextField textField = new TextField();
-        textField.setPromptText(promptText);
-        return textField;
-    }
-
-    public Label createLabel(String text) {
-        return new Label(text);
-    }
 }
