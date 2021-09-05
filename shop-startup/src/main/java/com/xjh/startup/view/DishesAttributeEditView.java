c3ffa2e36088a8fb7de3378e529744520facd94e
yuan6331 <271345425@qq.com>
2021-09-05 14:44:04 +0800

    add

--- shop-startup/src/main/java/com/xjh/startup/view/DishesAttributeEditView.java
+++ shop-startup/src/main/java/com/xjh/startup/view/DishesAttributeEditView.java
@@ -1,12 +1,17 @@
 package com.xjh.startup.view;
 
+import static com.xjh.common.utils.TableViewUtils.newCol;
+
 import com.xjh.common.valueobject.DishesAttributeVO;
+import com.xjh.common.valueobject.DishesAttributeValueVO;
 import com.xjh.startup.view.base.SmallForm;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class DishesAttributeEditView extends SmallForm {
@@ -34,6 +39,19 @@ public class DishesAttributeEditView extends SmallForm {
         markInput.setPrefWidth(200);
         addLine(newLine(markLabel, markInput));
 
+        TableView<DishesAttributeValueVO> attrValueTV = new TableView<>();
+        attrValueTV.getColumns().add(
+                newCol("属性值", "attributeValue", 100)
+        );
+        attrValueTV.setMaxHeight(200);
+        ObservableList<DishesAttributeValueVO> attrValues =
+                FXCollections.observableArrayList(attr.getAllAttributeValues());
+        attrValueTV.setItems(attrValues);
+        addLine(attrValueTV);
 
+        Button update = new Button("保存属性");
+        Button addAttr = new Button("增加属性");
+
+        addLine(newLine(addAttr, update));
     }
 }
