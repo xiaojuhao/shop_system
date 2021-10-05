7205f82cb8c66f0fa2196702a8cf24ce1fd6372a
yuan6331 <271345425@qq.com>
2021-10-05 22:52:13 +0800

    add

--- shop-common/src/main/java/com/xjh/common/valueobject/DishesAttributeVO.java
+++ shop-common/src/main/java/com/xjh/common/valueobject/DishesAttributeVO.java
@@ -7,12 +7,12 @@ import lombok.Data;
 
 @Data
 public class DishesAttributeVO {
-    Integer dishesAttributeId;
-    String dishesAttributeName;
-    String dishesAttributeMarkInfo;
+    Integer dishesAttributeId = 0;
+    String dishesAttributeName = "";
+    String dishesAttributeMarkInfo = "";
     Boolean isValueRadio = false;
     Boolean isSync = false;
-    Long createTime;
+    Long createTime = 0L;
     List<DishesAttributeValueVO> selectedAttributeValues = new ArrayList<>();
     List<DishesAttributeValueVO> allAttributeValues = new ArrayList<>();
 
