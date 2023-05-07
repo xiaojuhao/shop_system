d604935427e6fcdc48994045202b89b53f421644
yinguoliang <yglah126@126.com>
2023-05-07 23:41:52 +0800

    优化

--- shop-service/src/main/java/com/xjh/ws/WsHandlerModule.java
+++ shop-service/src/main/java/com/xjh/ws/WsHandlerModule.java
@@ -3,23 +3,7 @@ package com.xjh.ws;
 import com.google.inject.AbstractModule;
 import com.xjh.common.utils.Logger;
 import com.xjh.common.utils.TimeRecord;
-import com.xjh.ws.handler.AddCartHandler;
-import com.xjh.ws.handler.CheckDeskCartHandler;
-import com.xjh.ws.handler.CheckDeskInfoHandler;
-import com.xjh.ws.handler.ClearCartHandler;
-import com.xjh.ws.handler.CloseDeskHandler;
-import com.xjh.ws.handler.EraseHandler;
-import com.xjh.ws.handler.GetH5DeskInfoHandler;
-import com.xjh.ws.handler.GetPayCodeHandler;
-import com.xjh.ws.handler.GetPayInfoHandler;
-import com.xjh.ws.handler.GetUpdateDataPackageHandler;
-import com.xjh.ws.handler.H5ValidateHandler;
-import com.xjh.ws.handler.OpenDeskHandler;
-import com.xjh.ws.handler.OrderCartHandler;
-import com.xjh.ws.handler.RemoveDishesFromCartHandler;
-import com.xjh.ws.handler.ReturnDishesHandler;
-import com.xjh.ws.handler.SocketOpenHandler;
-import com.xjh.ws.handler.UpdateCartDishesHandler;
+import com.xjh.ws.handler.*;
 
 public class WsHandlerModule extends AbstractModule {
     @Override
@@ -42,6 +26,7 @@ public class WsHandlerModule extends AbstractModule {
         bind(ReturnDishesHandler.class);
         bind(SocketOpenHandler.class);
         bind(UpdateCartDishesHandler.class);
+        bind(LoginHandler.class);
         Logger.info("WebService模块注入 耗时:" + timeRecord.getCost());
     }
 }
