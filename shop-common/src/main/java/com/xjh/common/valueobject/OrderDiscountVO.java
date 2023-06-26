545d067e9acd6d48ff549a166a48a4af8548f882
yinguoliang <yglah126@126.com>
2023-06-26 23:38:59 +0800

    优化

--- shop-common/src/main/java/com/xjh/common/valueobject/OrderDiscountVO.java
+++ shop-common/src/main/java/com/xjh/common/valueobject/OrderDiscountVO.java
@@ -5,11 +5,11 @@ import lombok.Data;
 @Data
 public class OrderDiscountVO {
     private float rate;
-    private String discountName = "N";
+    private String discountName = "无";
     // com.xjh.common.enumeration.EnumDiscountType
     private int type;
     private int discountId;
-    private String discountCode = "N";
+    private String discountCode = "无";
     private int status;
     private int useTimes;
     private long startTime;
